9aeb3080b65eff8345d64adcee0206c9fa735c37
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-10-17 11:54:20 +0300

    add helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
+    def index
+    end
+    def new
+    end
+    def edit
+    end
     def show
-        @hello = "Привет Мишаня"
+        @user = User.new(
+            name: 'Roman',
+            username: 'Romashka',
+            avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100' 
+             )
     end
 end
