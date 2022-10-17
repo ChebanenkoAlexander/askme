8dc4069328464de8d91192f3a948be8117a620ad
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-10-17 13:41:37 +0300

    add partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,5 +11,9 @@ class UsersController < ApplicationController
             username: 'Romashka',
             avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100' 
              )
+             @questions = [ 
+                Question.new(text: 'Как дела?', created_at: Date.parse('26.03.1991')),
+                Question.new(text: 'Возьмете меня на работу?', created_at: Date.parse('26.03.1991'))
+             ]
     end
 end
