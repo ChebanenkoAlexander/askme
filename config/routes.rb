19ddf6ef8517ddae8b25aa5188ae2a551bb9e02a
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-10-17 10:39:19 +0300

    add buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'user#index'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 get 'show' => 'users#show'
   # Defines the root path route ("/")
