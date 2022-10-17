79bd268df015501d00e87b21c19ce3e27985211e
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-10-17 15:23:32 +0300

    upgreate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,12 @@ class UsersController < ApplicationController
         @user = User.new(
             name: 'Roman',
             username: 'Romashka',
-            avatar_url: 'https://secure.gravatar.com/avatar/71269686e0f757ddb4f73614f43ae445?s=100' 
+            #avatar_url: 'вставить ссылку на аватарку' 
              )
              @questions = [ 
                 Question.new(text: 'Как дела?', created_at: Date.parse('26.03.1991')),
                 Question.new(text: 'Возьмете меня на работу?', created_at: Date.parse('26.03.1991'))
              ]
+             @new_question = Question.new
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'user#index'
   resources :users
+  resources :questions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 get 'show' => 'users#show'
   # Defines the root path route ("/")
