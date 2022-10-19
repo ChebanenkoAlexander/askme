005c2f7aae0415137bd1ee875a358de7c850c99d
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-10-19 09:38:58 +0300

    user editing

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
    def encrypt_password
       if self.password.present?
          self.password_salt=User.hash_to_string(OpenSSL::Random.random_bytes(16))
-         self.password_hash+User.hash_to_string(OpenSSL::PKCS5.pdkdf2_hmac(self.password, self.password_salf, ITERATIONS, DIGEST.length, DIGEST))
+         self.password_hash=User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST))
 
       end
    end
