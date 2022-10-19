aa7ab1af39a34e4819d03a2d2ce6e36846cf0289
ChebanenkoAlexander <chebanenko56@gmail.com>
2022-10-19 13:25:03 +0300

    add_session

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ root 'users#index'
   resources :users, except: [:destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :questions
-
+  
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 end
